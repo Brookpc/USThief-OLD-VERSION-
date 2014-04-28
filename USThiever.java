605b414dde86cef985f444a7185a33209de2bc9d
Brookpc <bdunham24@gmail.com>
2014-04-28 15:31:50 -0400

    Update USThiever.java

--- USThiever.java
+++ USThiever.java
@@ -1,24 +1,41 @@
 import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
+import org.parabot.core.ui.components.LogArea;
 import org.parabot.environment.api.interfaces.Paintable;
 import org.parabot.environment.api.utils.Time;
 import org.parabot.environment.api.utils.Timer;
+import org.parabot.environment.input.Mouse;
 import org.parabot.environment.scripts.Category;
 import org.parabot.environment.scripts.Script;
 import org.parabot.environment.scripts.ScriptManifest;
 import org.parabot.environment.scripts.framework.Strategy;
+import org.rev317.api.wrappers.hud.Item;
+import org.rev317.api.wrappers.interactive.Npc;
+import org.rev317.api.wrappers.scene.Area;
+import org.rev317.api.wrappers.scene.SceneObject;
+import org.rev317.api.wrappers.scene.Tile;
+import org.rev317.api.wrappers.walking.TilePath;
 import org.rev317.api.events.MessageEvent;
-import org.parabot.environment.input.Mouse;
 import org.rev317.api.events.listeners.MessageListener;
 import org.rev317.api.methods.Calculations;
 import org.rev317.api.methods.Camera;
@@ -28,11 +45,8 @@ import org.rev317.api.methods.Npcs;
 import org.rev317.api.methods.Players;
 import org.rev317.api.methods.SceneObjects;
 import org.rev317.api.methods.Skill;
-import org.rev317.api.wrappers.hud.Item;
-import org.rev317.api.wrappers.interactive.Npc;
-import org.rev317.api.wrappers.scene.Area;
-import org.rev317.api.wrappers.scene.SceneObject;
-import org.rev317.api.wrappers.scene.Tile;
+
+import parabotp.USThiever.Gui;
 
 @ScriptManifest( author = "Brookpc", category = Category.THIEVING, description = "Steals and Sells items on UltimateScape 2", name = "USThiever", servers = { "UltimateScape" }, version = 3.0 )
 public class USThiever extends Script implements Paintable, MessageListener
@@ -42,24 +56,32 @@ public class USThiever extends Script implements Paintable, MessageListener
 	public static Area TA = new Area( new Tile( 2676, 3324, 0 ), new Tile( 2648, 3324, 0 ), new Tile( 2645, 3289, 0 ), new Tile( 2672, 3289, 0 ) );
 	public int npcID;
 	public int stallID;
-	public int startlvl;
+	public int startexp;
 	public int[] sellIDs = { 950, 1891, 1901, 2309, 958, 4658, 2007, 1641, 1639, 1643 ,1637};
 	public int curlvl;
-	public int lvlcount;
+	public int curexp;
+	public int expcount;
 	public int cashMade;
 	public int itemsStolen;
 	private final Color color1 = new Color( 229, 255, 59 );
 	private final Font font2 = new Font( "Arial", 0, 14 );
 	private final Timer RUNTIME = new Timer();
 	public static Image img1;
-
+	Gui g = new Gui();
+	public boolean guiWait = true;
+	public boolean paintWait = true;
 
 	@Override
 	public boolean onExecute()
 	{
-		img1 = getImage( "http://i.imgur.com/dQB0c5Y.png" );
-		startlvl = Skill.THIEVING.getLevel();
+		img1 = getImage( "http://i.imgur.com/mdXG5j4.png" );
+		g.setVisible(true);
+		while (guiWait) {
+			Time.sleep(200);
+		}
+		startexp = Skill.THIEVING.getExperience();
 		curlvl = Skill.THIEVING.getLevel();
+		curexp = Skill.THIEVING.getExperience();
 		if( curlvl < 20 ) {
 			stallID = 1616;
 		} // Bread
@@ -98,10 +120,10 @@ public class USThiever extends Script implements Paintable, MessageListener
 	}
 
 
-	public void atlvlchange()
+	public void atexpchange()
 	{
-		curlvl = Skill.THIEVING.getLevel();
-		lvlcount = ( curlvl - startlvl );
+		curexp = Skill.THIEVING.getExperience();
+		expcount = ( curexp - startexp );
 		return;
 	}
 
@@ -158,7 +180,7 @@ public class USThiever extends Script implements Paintable, MessageListener
 		@Override
 		public void execute()
 		{
-			atlvlchange();
+			atexpchange();
 			for( SceneObject i: SceneObjects.getNearest( stallID ) ) {
 				;
 				if(i != null &&  i.isOnScreen() ) {
@@ -265,7 +287,7 @@ public class USThiever extends Script implements Paintable, MessageListener
 		} else if (me.getMessage().contains("steal ruby ring(s)")) {
 			itemsStolen += 1;
 			cashMade += 31000;
-		} else if (me.getMessage().contains("steal sappire ring(s)")) {
+		} else if (me.getMessage().contains("steal sapphire ring(s)")) {
 			itemsStolen += 1;
 			cashMade += 25000;
 		} else if (me.getMessage().contains("steal emerald ring(s).")) {
@@ -282,10 +304,67 @@ public class USThiever extends Script implements Paintable, MessageListener
 		g.drawImage(img1, 4, 23, null);
 		g.setFont(font2);
 		g.setColor(color1);
-		g.drawString( "" + lvlcount, 82, 57);
+		g.drawString( "" + expcount, 82, 57);
 		g.drawString( "" + RUNTIME, 82, 83);
 		g.drawString( "" + cashMade, 82, 70);
 
 	}
+	public class Gui extends JFrame {
 
+		private JPanel contentPane;
+
+		public void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						Gui frame = new Gui();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+
+		public Gui() {
+			initComponents();
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 150, 180);
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
+
+			lblUSChop = new JLabel("USThief");
+			lblUSChop.setFont(new Font("Arial", Font.PLAIN, 20));
+			lblUSChop.setBounds(15, 11, 101, 16);
+			contentPane.add(lblUSChop);
+
+			lblWhatTree = new JLabel("Ready?");
+			lblWhatTree.setBounds(17, 49, 82, 14);
+			contentPane.add(lblWhatTree);
+
+
+			btnStart = new JButton("Start");
+			btnStart.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					guiWait= false;
+					g.dispose();
+				}
+			});
+			btnStart.setBounds(10, 112, 95, 23);
+			contentPane.add(btnStart);
+		}
+		private void initComponents() {
+			lblUSChop = new JLabel();
+			lblWhatTree = new JLabel();
+			treeToChop = new JComboBox();
+
+		}
+		private JLabel lblUSChop;
+		private JButton btnStart;
+		private JComboBox treeToChop;
+		private JLabel lblWhatTree;
+
+	}
 }
